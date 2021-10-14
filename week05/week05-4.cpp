20ec92b11a45cd74f11968410eb73dee26604852
曾惟筠 <91250174+ww218@users.noreply.github.com>
2021-10-14 17:37:46 +0800

    Add files via upload

---
+++ week05/week05-4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    if (-2) printf("-2成立\n");
+    if (-1) printf("-1成立\n");
+    if (0) printf("0不成立\n");
+    if (1) printf("1成立\n");
+    if (2) printf("2成立\n");
+    if (3) printf("3成立\n");
+}
